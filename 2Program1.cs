ed52b25fbc89757e027968f64dfee4be76e8ba91
1Ruslan3 <148198692+1Ruslan3@users.noreply.github.com>
2023-12-07 11:13:55 +0300

    Add files via upload

---
+++ 2Program1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace St
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            try
+            {
+                var student = new Student("1222", "2", "3", "4", 3, "12345");
+                object student2 = new Student("123", "12", "1", "1", 3, "1");
+                var rbn = student.RecordBookNumber;
+
+                Console.WriteLine(student2.Equals(student));
+
+                foreach (var fieldValue in student)
+                {
+                    Console.WriteLine(fieldValue);
+                }
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (AggregateException ex)
+            {
+                foreach (var e in ex.InnerExceptions)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+    }
+}
